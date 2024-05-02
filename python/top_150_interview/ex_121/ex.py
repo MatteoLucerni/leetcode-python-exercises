7d364742b403f0c0e00e7bb635d5f28d85fee3dc
Matteo Lucerni <matteo.lucerni@kelony.com>
2024-05-02 12:14:01 +0200

    ex 122

--- python/top_150_interview/ex_121/ex.py
+++ python/top_150_interview/ex_121/ex.py
@@ -4,20 +4,27 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-
-        min_price = float("inf")
         max_profit = 0
-
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
-
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
         return max_profit
 
 
+# Ex1
 sol = Solution()
 
 prices = [7, 1, 5, 3, 6, 4]
+print(sol.maxProfit(prices))
+
+# Ex2
+sol = Solution()
+
+prices = [1, 2, 3, 4, 5]
+print(sol.maxProfit(prices))
+
+# Ex3
+sol = Solution()
+
+prices = [7, 6, 4, 3, 1]
 print(sol.maxProfit(prices))
